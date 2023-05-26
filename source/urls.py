42ddbd9b0593be14c811acb6d71d50d7ad421bf6
Kideri <kiderikun@gmail.com>
2023-05-26 21:56:54 +0300

    Fixed docs

--- source/urls.py
+++ source/urls.py
@@ -20,11 +20,13 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 
 schema_view = get_schema_view(
     openapi.Info(title="Dolgostoiki-20 API", default_version="v1"),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
