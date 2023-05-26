3979a5f8008c4763617e92aa78e4fb2eaecd1037
Kideri <kiderikun@gmail.com>
2023-05-26 11:21:14 +0300

    Fixed tests

--- user/tests/tests_get_user_info.py
+++ user/tests/tests_get_user_info.py
@@ -53,7 +53,6 @@ class TestOtherUserInfo(BaseGameCoreTestCase):
         assert "last_seen" in response_data
         expected = {
             "id": self.other_user.id,
-            "first_name": None,
             "last_seen": response_data["last_seen"],
             "role": "D"
         }
